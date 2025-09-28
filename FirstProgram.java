00112764837c941c46de2c919275b00dd6f332ba
vaishu221613 <vaishnavibhovi94@gmail.com>
2025-09-28 14:10:03 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,14 @@
+package vaish.java;
+
+public class FirstProgram {
+
+	public FirstProgram() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}

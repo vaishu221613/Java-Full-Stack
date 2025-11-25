b027bbcfe1befa9b1dc58958f2fce0ebd17b51cf
vaishu221613 <vaishnavibhovi94@gmail.com>
2025-11-25 21:36:35 +0530

    Add files via upload

---
+++ FinalMethodDemo.java
@@ -0,0 +1,17 @@
+package finaldemo;
+
+public class FinalMethodDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			
+		//Final methods can't be overrided
+		/*
+		 * @Override void show() { }
+		 */
+		
+
+	}
+
+}
